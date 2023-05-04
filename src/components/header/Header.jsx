f3359738db3f57bd5b6a32c0c2c5994802249efa
Hunter Padgett <gizmokabanjo@gmail.com>
2023-05-03 22:11:47 -0400

    style update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ import {
 const Header = () => {
  window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
-  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  if (this.scrollY >= 95) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
  });
 
